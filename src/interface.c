f49f28a64bdd41e31b6dd141edcec9dbeecc8265
Paul Geerken <paul.geerken@gmx.de>
2024-02-16 14:20:54 +0100

    interface rendering work

--- src/interface.c
+++ src/interface.c
@@ -1,7 +1,10 @@
 #include "interface.h"
 #include "SDL_render.h"
+#include "SDL_surface.h"
 #include "SDL_ttf.h"
-#include "SDL_video.h"
+
+SDL_Color in_bg = {20, 20, 20, 255};
+SDL_Color in_fg = {200, 200, 200, 255};
 
 interface_t in_create(uint8_t grid_w, uint8_t grid_h, uint8_t ptsize) {
   interface_t res;
@@ -40,8 +43,18 @@ interface_t in_create(uint8_t grid_w, uint8_t grid_h, uint8_t ptsize) {
                 SDL_GetError());
   }
 
+  SDL_SetWindowTitle(res.win, "Rouge Galaxy");
+
   // init Grid
-  res.grid = malloc(res.w * res.h * sizeof(char));
+  size_t mem_size = res.w * res.h * sizeof(char);
+
+  res.grid = malloc(mem_size);
+  memset(res.grid, '.', mem_size);
+
+  // format grid array so it can be drawn directly
+  /* for(int i = 0; i< ((res.w+1)*res.h)-1;++i){ */
+  /*   if(i % res.w == 0)res.grid[i+1] = '\n'; */
+  /* } */
 
   return res;
 }
@@ -62,6 +75,23 @@ void in_drawAt(interface_t *in, char c, ivec2_t pos) {
 
   in->grid[index] = c;
 }
-void in_drawPresent(interface_t *in) { SDL_RenderPresent(in->r); }
+void in_drawPresent(interface_t *in) {
+  // Draw gird text to Surface
+  Uint32 wl = in->grid_cell_w * in->w;
+  SDL_Surface *s =
+      TTF_RenderText_Shaded_Wrapped(in->f, in->grid, in_fg, in_bg, wl);
 
-void in_clearScreen(interface_t *in) { SDL_RenderClear(in->r); }
+  // Create Texture & render
+  SDL_Texture *t = SDL_CreateTextureFromSurface(in->r, s);
+
+  SDL_RenderCopy(in->r, t, NULL, NULL);
+  SDL_RenderPresent(in->r);
+
+  SDL_FreeSurface(s);
+  SDL_DestroyTexture(t);
+}
+
+void in_clearScreen(interface_t *in) {
+  SDL_SetRenderDrawColor(in->r, in_bg.r, in_bg.g, in_bg.b, in_bg.a);
+  SDL_RenderClear(in->r);
+}
