9f49b5fa14d5633e95e549f160ca327df17e379f
Sebastian Brücker <sebastian.bruecker@rwth-aachen.de>
2024-02-17 23:02:22 +0100

    wafe function collapse algorithm

---
+++ src/wfc.h
@@ -0,0 +1,17 @@
+#ifndef WFC_H_
+#define WFC_H_
+
+#include <stdbool.h>
+
+typedef struct wfcdata {
+  bool ***tilemap; 
+  bool ***constraints; 
+  int n_constraints; 
+  int map_width;
+  int map_height; 
+} wfcdata_t;
+
+
+void wave_function_collapse(wfcdata_t *data, int startY, int startX);
+
+#endif // WFC_H_
